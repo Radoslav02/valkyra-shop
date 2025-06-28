db09b27286adc7413bead39e2153d62d5e6289be
Radoslav02 <radoslavpavkovg@gmail.com>
2025-06-28 11:57:32 +0200

    footer fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -41,7 +41,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2024 PR. All rights reserved.</p>
+        <p>&copy; 2025 PR. All rights reserved.</p>
       </div>
     </footer>
   );
